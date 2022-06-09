3fae1f9ef4551b1f1efb97df8c89d156e720d717
Yui21 <84171257+Yui21@users.noreply.github.com>
2022-06-10 01:36:23 +0900

    20220610

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 const getData = async () => {
     const endpoint =
-        "https://script.google.com/macros/s/AKfycbxYb6A56yxS_gLG_AkWxMODItAzBrzYYT8CT3Yvxel3UlgNhau-sJnH1ZbFM-Ho_GcQkA/exec";
+        "https://script.google.com/macros/s/AKfycbxYb6A56yxS_gLG_AkWxMODItAzBrzYYT8CT3Yvxel3UlgNhau-sJnH1ZbFM-Ho_GcQkA/exec?sheet=group3";
     try {
         const response = await fetch(endpoint);
         if (response.ok) {
@@ -22,24 +22,25 @@ const renderResponse = (res) => {
 
     const venues = document.getElementById("venues");
 
-    for (const venue of res) {
+    for (const venue of res.filter(d => d.name !== '')) {
         const photo = venue.photo !== '' ? venue.photo : 'images/placeholder.png';
         const venueNode = document.createElement("li");
         venueNode.className = 'column is-one-third';  //is-half=半分、is-one-third:1/3
 
         venueNode.innerHTML = `
         <div class = "card">
-          <div class = "card-image">
-            <figure class = "image is-one-third">
-              
-              <h3 class = subtitle is-6>
+          <div class = "text-area">
+            <h3 class = subtitle is-6>
               ${venue.name}
-              </h3>
+            </h3>
+            <figure class = "image is-one-third">
+
+
             </figure>
           </div>
 
-          
-        
+
+
           <div class = "content">
           </div>
 

9a41b41a3a6ac3c0d56442ee1eb744907fbef360
Yusaku Tsue <84454852+20144079@users.noreply.github.com>
2022-06-12 00:06:28 +0900

    Add files via upload

--- docs/event/index.js
+++ docs/event/index.js
@@ -1,85 +1,52 @@
 const getData = async () => {
-  const endpoint =
-    "https://script.google.com/macros/s/AKfycbxhZ4ww0rLhp6A72xu4HznL5g-cA6BqosnggI2xlzzqrQKqVbq2HTLZO8MpdnaIkZLG_Q/exec?sheet=group3";
-  try {
-    const response = await fetch(endpoint);
-    if (response.ok) {
-      const json = await response.json();
-      return json;
+    const endpoint =
+        "https://script.google.com/macros/s/AKfycbxYb6A56yxS_gLG_AkWxMODItAzBrzYYT8CT3Yvxel3UlgNhau-sJnH1ZbFM-Ho_GcQkA/exec?sheet=group3";
+    try {
+        const response = await fetch(endpoint);
+        if (response.ok) {
+            const json = await response.json();
+            return json;
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
 
+const renderResponse = (res) => {
+    //変なボックス削除
+    //document.getElementById("response").value = JSON.stringify(res, null, 2);
 
+    document.getElementsByClassName(
+        "split right-box"
+    )[0].innerHTML = `<ul id="venues" class="columns is-multiline is-gapless"></ul>`;  //is-multilineで折り返す、is-gaplessで余白無し
 
-// urlにパラメータ
-const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+    const venues = document.getElementById("venues");
 
+    for (const venue of res.filter(d => d.name !== '')) {
+        const photo = venue.photo !== '' ? venue.photo : 'images/placeholder.png';
+        const venueNode = document.createElement("li");
+        venueNode.className = 'column is-one-third';  //is-half=半分、is-one-third:1/3
 
-
-
-const renderEventList = (json) => {
-  document.getElementById("content").innerHTML =
-    `<ul id="events" class="columns is-multiline"></ul>`;
-
-  const events = document.getElementById("events");
-  for (const event of json.filter(d => d.name !== '')) {
-    const startDate = DateTime.fromJSDate(new Date(event.startDate));
-    const endDate = DateTime.fromJSDate(new Date(event.endDate));
-    const thumbnail = event.thumbnail !== '' ? event.thumbnail : '../images/placeholder.png';
-    const item = document.createElement("li");
-    item.className = 'column is-one-quarter';
-    item.innerHTML = `
-      <div class="card">
-      <div class="card-content">
-          <h3 class="title is-5">
-            <a href="event/?id=${event.id}">${event.title}</a>
-          </h3>
-          <div class="content">
-          ${event.venue}
-          <div>
-          <div class="content is-small">
-            ${startDate.setLocale('ja').toFormat('yyyy.MM.dd')} -
-            ${endDate.setLocale('ja').toFormat('yyyy.MM.dd')}
+        venueNode.innerHTML = `
+        <div class = "card">
+          <div class = "text-area">
+            <h3 class = subtitle is-6>
+              ${venue.name}
+            </h3>
+            <figure class = "image is-one-third">
+            </figure>
           </div>
-          <a href="event/?id=${event.id}" class="event-link">
-            <figure class="image is-square is-one-quarter poster" style="background-image:url(${thumbnail})">
+          <div class = "content">
+          </div>
+          <a href = "#">
+            <figure class="image is-square is-one-quarter poster" style="background-image:url(${photo})">
             </figure>
           </a>
         </div>
+        
       </div>`;
-    events.appendChild(item);
-  }
-}
-
-const renderEvent = (json) => {
-  console.dir(json);
-  if (!id) {  //もしidがなかったら
-    renderEventList(json);
-    const eventLinks = document.querySelectorAll('.event-link');
-    for (const eventLink of eventLinks) {
-      eventLink.onmouseover = (e) => {
-        document.querySelector('.preview').style.backgroundImage = e.target.style.backgroundImage;
-      };
+        venues.appendChild(venueNode);
     }
-    return;
-  }
-
-  //個別のイベントを表示
-  const event = json.find((d) => d.id === id);  //idが完全に一致していたら
-  if (event) {
-    document.title = `${event.title} | tama.potari`;
-    document.getElementById("content").innerHTML = `
-      <h1>${event.event1}</h1>
-      <h1>${event.openingTime}</h1>
-      <h1>${event.station}</h1>
-
-      
-      `;
-  }
 };
 
-getData('events').then((json) => renderEvent(json));
+getData().then((json) => renderResponse(json));

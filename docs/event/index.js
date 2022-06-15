1e0e419b2f8fe816de0b60928cfc64d15f9ea914
Yusaku Tsue <84454852+20144079@users.noreply.github.com>
2022-06-15 22:39:35 +0900

    Add files via upload

--- docs/event/index.js
+++ docs/event/index.js
@@ -1,6 +1,6 @@
 const getData = async () => {
   const endpoint =
-    "https://script.googleusercontent.com/macros/echo?user_content_key=l7eaf9Pz5HsRoUnKGKSmmGpHJDHOxodUyfQpfS7F0iKKIpGX_CKIZ0SsYoM3LIVH7MsWAYyl510r-VO7YHE3shRKsEsi8xfkm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEmHHJxhC_oe1Qmd2R-eXjoXgTxWUu4HYlJom6QacPSgNEmyKwSz32FPG-bn2sJSQBMlTA-c0F3yHHty0meKf-_VOxuX8xhGctz9Jw9Md8uu&lib=MabRb0sHcOdgcukW2MiMwBlocHvvcqee0";
+    "https://script.googleusercontent.com/macros/echo?user_content_key=U9f2NcUmhV0KMZe97gdZyV7ODzsSwDcxWJTP5T-cGCegMWq3yfDgd1j-j5YDlMptPmOrvuDhoS0lNzJi01wuM_oUbLHEqj_Tm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGZdoPlb0IGml4_OMP16J45dKse2zz4h-WDBTs9xzMk7kkFz-Bu9aE-kz-ysEQ868d3uVCuwDB3jDPuUtGg-AA_8IR5cXPekpO6QzIpnjC14TjygBgXw9YE&lib=MabRb0sHcOdgcukW2MiMwBlocHvvcqee0";
 
     try {
     const response = await fetch(endpoint);
@@ -13,21 +13,27 @@ const getData = async () => {
   }
 };
 
+
+
 // urlにパラメータ
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
+
+
 
 const renderResponse = (json) => {
 
     for (const event of json.filter(d => d.id == id)) {
 		document.getElementById("content").innerHTML =`
-      <h1>${event.name}</h1>
+      <h1>${event.venue}</h1>
+      <img src = "${event.thumbnail}">
       <p>住所： ${event.address}</p>
       <p>最寄駅： ${event.station}</p>
       <p>アクセス： ${event.access}</p>
       <p>休館日： ${event.closingDay}</p>
       <p>開館時間： ${event.openingTime}</p>
       <p>電話番号： ${event.telephone}</p>
+      <p><a href = "${event.url}">ホームページ</a></p>
       `;
     }
 };
